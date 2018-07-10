ec551d6ced7ad02d37d5b687d4aac5eea16bec43
Eric Pelland <ericpelland94@gmail.com>
2018-07-10 08:34:53 -0400

    package.json, angular.json, and app component test changes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to rosinlog!');
+    expect(page.getParagraphText()).toEqual('Welcome to Angular-6-Base-Project!');
   });
 });
